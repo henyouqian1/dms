3cc6693ec60f83917fcbef1ccc3ae98ebc801586
liwei <henyouqian@hotmail.com>
2011-11-23 11:54:14 +0800

    readd

---
+++ dmsDevView.py
@@ -0,0 +1,22 @@
+﻿from dmsDev import *
+
+@devBluePrint.route('/dms/devview')
+def devview():
+    return render_template('dmsDev.html')
+    
+@devBluePrint.route('/dms/devloginview')
+def devloginview():
+    return jsonify(view=render_template('dmsDevLogin.html'))
+    
+@devBluePrint.route('/dms/devregisterview')
+def devregisterview():
+	return jsonify(view=render_template('dmsDevRegister.html'))
+	
+@devBluePrint.route('/dms/devmainmenuview')
+def devmainmenuview():
+	return jsonify(view=render_template('dmsDevMainMenu.html'))
+	
+@devBluePrint.route('/dms/devawardview')
+def devawardview():
+	return jsonify(view=render_template('dmsDevAward.html'), addview=render_template('dmsDevAddAward.html'), editview=render_template('dmsDevEditAward.html'))
+	
